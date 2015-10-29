40b687b2bfdb10b33df86391c1d3e1c520be253e
johny babylon <ztredded@gmail.com>
2015-10-28 22:59:37 -0700

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by elliott chavis on 10/28/15.
+//  Copyright (c) 2015 elliott chavis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
