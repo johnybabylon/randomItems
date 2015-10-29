c92803cca9e0240a97d7bc6c6e20dc6ae3bec0b4
johny babylon <ztredded@gmail.com>
2015-10-29 12:51:01 -0700

    first

---
+++ RandomItems/BNRItem.h
@@ -0,0 +1,39 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by elliott chavis on 10/29/15.
+//  Copyright (c) 2015 elliott chavis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
++ (instancetype)randomItem;
+
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
--- RandomItems/main.m
+++ RandomItems/main.m
@@ -7,11 +7,57 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        // Send the message addObject: to the NSMutableArray pointed to
+        // by the variable items, passing a string each time
+        
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        // Send another message, insertObject:atIndex:, to that same array object
+        [items insertObject:@"Zero" atIndex:0];
+        [items insertObject:@"four" atIndex:4];
+
+        
+        
+        // For every item in the items array ...
+        for (NSString *item in items) {
+            // Log the description of the item
+            NSLog(@"%@", item);
+        }
+        
+        BNRItem *item = [[BNRItem alloc] initWithItemName:@"Red Sofa"
+                                           valueInDollars:100
+                                             serialNumber:@"A1B2C"];
+        
+        
+        // The %@ token is replaced with the result of sending
+        // the description message to the corresponding argument
+        NSLog(@"%@", item);
+        
+        BNRItem *itemWithName = [[BNRItem alloc] initWithItemName:@"Blue Sofa"];
+        NSLog(@"%@", itemWithName);
+        
+        BNRItem *itemWithNoName = [[BNRItem alloc] init];
+        NSLog(@"%@", itemWithNoName);
+        
+
+        
+        
+        
+        // Destroy the mutable array object
+        items = nil;
+
+
     }
     return 0;
 }
